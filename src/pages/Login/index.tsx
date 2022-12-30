0ef63fc74001a0192df2a80fbb37aa3a417194e9
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-12-30 14:35:13 -0300

    Criando página de Login 🖥️

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+import { Title } from "../../components/Title/styles"
+
+export function Login(){
+  return(
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  )
+}
