e4df6f45d8ce049fdbf84856d61f64bfe389e2de
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-12-30 14:33:20 -0300

    criando página de erro ❗

---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,19 @@
+import { StorysetImage } from "../../components/StorysetImage"
+import { Title } from '../../components/Title/styles'
+
+import { Container } from "./styles"
+
+import NotFoundImage from '../../assets/Page-not-Found.svg'
+
+export function ErrorPage(){
+  return(
+    <Container>
+      <Title>Página não encontrada</Title>
+      <StorysetImage
+        image={NotFoundImage}
+        link="https://storyset.com/not%20found"
+        message="Not Found Illustrations by Storyset"
+      />
+    </Container>
+  )
+}
