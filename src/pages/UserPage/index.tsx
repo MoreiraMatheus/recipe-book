bcc7bc69b6386f30a599a591835f98208a6825df
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2023-01-31 13:40:35 -0300

    estilizando tela de user

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -18,11 +18,17 @@ export function UserPage() {
 			<Title>{userName}</Title>
 			<div>
 				<div>
+					<img src="https://upload.wikimedia.org/wikipedia/commons/6/67/Tuxgaucho.svg" alt="foto user" />
 					<p>Receitas salvas: {savedRecipes}</p>
 					<p>Receitas publicadas: {publishedRecipes}</p>
+					<p>Local: </p>
+					<p>Por aqui desde:</p>
+					<p>Seguidores: </p>
+					<p>Seguindo: </p>
 				</div>
+				<hr />
 				<Button
-					extraCSS='padding: 8px'
+					extraCSS='padding: 8px;'
 					width='80%'
 					onClick={() => {
 						setUserName('');
